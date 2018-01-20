f21d0914d20e88a6c661545ab243f5b723aef25c
Pawel Wlazlo <mrogur@gmail.com>
2018-01-20 10:37:22 +0100

    upgraded to bootstrap 4.0.0 final added custom action to featbox

--- functions.php
+++ functions.php
@@ -128,3 +128,25 @@ function trendone_get_image_thumb($thumbnailDimension, $postId=null) {
 
     return wp_get_attachment_image_src($attachment_id, $thumbnailDimension, null);
 }
+
+
+/**
+ * Register terms for FeatBox
+ */
+
+function featbox_register_terms()
+{
+    if (wp_count_terms("featbox") > 0) {
+        return;
+    }
+
+    wp_insert_term(_('First Featured Box'), 'featbox', [
+        'slug' => 'box_down'
+    ]);
+    wp_insert_term(_('Second Featured Box'), 'featbox', [
+        'slug' => 'box_up'
+    ]);
+}
+
+add_action('featbox_register_terms', 'featbox_register_terms');
+
--- modules/taxonomies/featboxes.php
+++ modules/taxonomies/featboxes.php
@@ -6,32 +6,34 @@
  * Time: 17:42
  */
 
-add_action( 'init', 'create_featbox_taxonomy' );
+add_action('init', 'create_featbox_taxonomy');
+add_action('init', 'featbox_register_terms_action');
 
-function create_featbox_taxonomy() {
+function create_featbox_taxonomy()
+{
     $labels = array(
-        'name'                           => 'Featboxes',
-        'singular_name'                  => 'Featbox',
-        'search_items'                   => 'Search Featboxes',
-        'all_items'                      => 'All Featboxes',
-        'edit_item'                      => 'Edit Featbox',
-        'update_item'                    => 'Update Featbox',
-        'add_new_item'                   => 'Add New Featbox',
-        'new_item_name'                  => 'New Featbox Name',
-        'menu_name'                      => 'Featbox',
-        'view_item'                      => 'View Featbox',
-        'popular_items'                  => 'Popular Featbox',
-        'separate_items_with_commas'     => 'Separate featboxes with commas',
-        'add_or_remove_items'            => 'Add or remove featboxes',
-        'choose_from_most_used'          => 'Choose from the most used featboxes',
-        'not_found'                      => 'No featboxes found'
+        'name' => 'Featboxes',
+        'singular_name' => 'Featbox',
+        'search_items' => 'Search Featboxes',
+        'all_items' => 'All Featboxes',
+        'edit_item' => 'Edit Featbox',
+        'update_item' => 'Update Featbox',
+        'add_new_item' => 'Add New Featbox',
+        'new_item_name' => 'New Featbox Name',
+        'menu_name' => 'Featbox',
+        'view_item' => 'View Featbox',
+        'popular_items' => 'Popular Featbox',
+        'separate_items_with_commas' => 'Separate featboxes with commas',
+        'add_or_remove_items' => 'Add or remove featboxes',
+        'choose_from_most_used' => 'Choose from the most used featboxes',
+        'not_found' => 'No featboxes found'
     );
 
     register_taxonomy(
         'featbox',
         'post',
         array(
-            'label' => __( 'Featbox' ),
+            'label' => __('Featbox'),
             'hierarchical' => true,
             'show_admin_column' => true,
             'show_in_nav_menus' => true,
@@ -45,3 +47,9 @@ function create_featbox_taxonomy() {
         )
     );
 }
+
+function featbox_register_terms_action()
+{
+  do_action('featbox_register_terms');
+}
+
